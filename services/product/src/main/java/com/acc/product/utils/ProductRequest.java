d316604f9062adb8d4075ca52a59244a93e1880b
Jardeylson Jacinto <jardeylsong.m@gmail.com>
2025-05-15 11:36:26 -0300

    feat: add product request

---
+++ services/product/src/main/java/com/acc/product/utils/ProductRequest.java
@@ -0,0 +1,16 @@
+package com.acc.product.utils;
+
+import java.math.BigDecimal;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record ProductRequest(
+
+    Integer id,
+    @NotNull(message = "Product name is required") String name,
+    @NotNull(message = "Product description is required") String description,
+    @Positive(message = "Available quantity should be positive") double availableQuantity,
+    @Positive(message = "Price should be positive") BigDecimal price,
+    @NotNull(message = "Product category is required") Integer categoryId) {
+}
