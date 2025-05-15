8f3f6a63d035ccd3b14d79a550e79203978f0921
Jardeylson Jacinto <jardeylsong.m@gmail.com>
2025-05-15 12:03:37 -0300

    feat: add product purchase request

---
+++ services/product/src/main/java/com/acc/product/utils/ProductPurchaseRequest.java
@@ -0,0 +1,9 @@
+package com.acc.product.utils;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record ProductPurchaseRequest(
+    @NotNull(message = "Product is mandatory") Integer productId,
+    @Positive(message = "Quantity is mandatory") double quantity) {
+}
